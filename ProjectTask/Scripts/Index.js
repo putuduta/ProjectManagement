3e2f07c090eb7fa053e311349fb98d590decb534
putuduta <putu.duta@gmail.com>
2021-01-20 14:16:47 +0700

    fix ajax jquery

---
+++ ProjectTask/Scripts/Index.js
@@ -0,0 +1,97 @@
+﻿function myFunction() {
+
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("ProjectList");
+    tr = table.getElementsByTagName("tr");
+
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByClassName("ProjectName")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+$('.group').on('change', function () {
+    var totalSeen = $("input.group:checked").length;
+    if ($(this).prop("checked")) {
+        var id = $(this).val();
+        $("#formAddSecur").submit();
+
+    }
+});
+
+$('#formAddSecur').submit((e) => {
+    e.preventDefault();
+    var data = $('#formAddSecur').serialize();
+    console.log(data);
+    $.ajax({
+        type: "POST",
+        url: "Home/StoreAuthUser",
+        data: data,
+        success: (data) => {
+            console.log("sukses");
+            if (data.success) {
+                alert("Success Add Authorized User")
+            } else {
+                alert("Cannot Add Authorized User")
+            }
+        },
+        error: (err) => {
+            alert(err.status);
+        }
+
+    });
+});
+
+console.log(status);
+
+$(".addProject").click(function () {
+    var id = $(this).data('id');
+    console.log(id);
+    $('#formAddSecur #ProjectID').val(id);
+});
+
+
+function AddProject() {
+    var data = $('#formAddProject').serialize();
+    $.ajax({
+        type: "POST",
+        url: "Home/Store",
+        data: data,
+        success: (data) => {
+            if (data.success) {
+                alert("Success Add Project")
+                $('#ProjectList').load('/Home #ProjectList');
+            } else {
+                alert("Cannot Add Project")
+            }
+        }
+
+    });
+}
+
+
+$('#formAddProject').validate({
+    rules: {
+        ProjectName: {
+            required: true,
+        }
+    }
+});
+
+$(document).ready(function () {
+    $(".link").click(function (event) {
+        var $this = $(this),
+            url = $this.val();
+        window.location.href = url;
+    });
+});
---
+++ ProjectTask/Scripts/UserManagement.js
@@ -0,0 +1,164 @@
+﻿$('#formAddUser').validate({
+    rules: {
+        Username: {
+            required: true
+        },
+        UserPassword: {
+            required: true,
+            alphanumeric: true
+        },
+        ImageUpload: {
+            required: true,
+            extension: "jpeg|gif|png"
+        }
+    }
+});
+
+
+$("#mySearch").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#List tr").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+});
+
+
+
+
+
+$('.fileUpload-2').on('change', function () {
+    //get the file name
+    var fileName = $(this).val();
+    //replace the "Choose a file" label
+    $(this).next('.custom-file-label').html(fileName);
+})
+
+$('.fileUpload-1').on('change', function () {
+    //get the file name
+    var fileName = $(this).val();
+    //replace the "Choose a file" label
+    $(this).next('.custom-file-label').html(fileName);
+})
+
+
+function AjaxPost(formData) {
+    var ajaxConfig = {
+        type: "post",
+        url: "/Home/AddUser",
+        data: new FormData(formData),
+        success: function (result) {
+            alert("Success Add User");
+            $('#tableList').load('/Home/UserManagement #tableList');
+        },
+        error: function (err) {
+            alert("Error Add User");
+            console.log(err);
+        }
+    }
+    if ($(formData).attr('enctype') == "multipart/form-data") {
+        ajaxConfig["contentType"] = false;
+        ajaxConfig["processData"] = false;
+    }
+
+    $.ajax(ajaxConfig);
+    return false;
+}
+
+console.log(status);
+
+$(".addUser").click(function () {
+
+    $('#UsernameInput').empty();
+    $('#PasswordInput').empty();
+    $('#RolesInput').empty();
+    $('#PhotoInput').empty();
+    $('#StatusInput').empty();
+
+    var id = $(this).data('id');
+    console.log("masuk");
+    console.log(id);
+    $('.modal-body #UserID').val(id);
+
+    $.ajax({
+        type: "GET",
+        url: "/Home/GetOneUser",
+        data: {
+            id: id
+        },
+        success: function (result) {
+            $('#UsernameInput').append(`<input type="text" class="form-control mt-2" id="EditUsername" name="Username" placeholder="${result.Username}" value="${result.Username}" readonly>`);
+            $('#PasswordInput').append(`<input type="password" class="form-control mt-2" id="EditUserPassword" name="UserPassword" value="${result.UserPassword}" required>`);
+
+            if (result.UserRoles === "Admin") {
+                $('#RolesInput').append(
+                    `<select id="UserRoles" name="UserRoles" class="form-control mt-2">
+                                        <option  value="Admin" selected>Admin</option>
+                                        <option value="User">User</option>
+                                    </select>`
+                );
+            } else if (result.UserRoles === "User") {
+                $('#RolesInput').append(
+                    `<select id="UserRoles" name="UserRoles" class="form-control mt-2">
+                                        <option value="User" selected >User</option>
+                                        <option value="Admin">Admin</option>
+                                    </select>`
+                );
+            }
+
+            $('#PhotoInput').append(
+                `<img src="/AppFile/Images/${result.UserPhoto}" style="width: 100px; padding-right: 0px; height: 120px; " alt="No Images">
+                                        <input type="text" id="UserPhoto" name="UserPhoto" value="${result.UserPhoto}" hidden>`
+            );
+
+
+
+            if (result.UserStatus === "Active") {
+                $('#StatusInput').append(
+                    `<input type="checkbox" class="custom-control-input" id="UserStatus" name="UserStatus" value="Active" checked>
+                                     <label class="custom-control-label" for="UserStatus">Active</label>`
+                );
+            } else {
+                $('#StatusInput').append(
+                    `<input type="checkbox" class="custom-control-input" id="UserStatus" name="UserStatus" value="Active">
+                                     <label class="custom-control-label" for="UserStatus">Active</label>`
+                );
+            }
+
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    })
+});
+
+function EditPost(formData) {
+    var ajaxConfig = {
+        type: "post",
+        url: "/Home/EditUser",
+        data: new FormData(formData),
+        success: function (result) {
+            alert("Success Edit User");
+            $('#tableList').load('/Home/UserManagement #tableList');
+        },
+        error: function (err) {
+            alert("Error Edit User");
+            console.log(err);
+        }
+    }
+    if ($(formData).attr('enctype') == "multipart/form-data") {
+        ajaxConfig["contentType"] = false;
+        ajaxConfig["processData"] = false;
+    }
+
+    $.ajax(ajaxConfig);
+    return false;
+}
+
+$(document).ready(function () {
+    $(".link").click(function (event) {
+        var $this = $(this),
+            url = $this.val();
+        window.location.href = url;
+    });
+});
